bd91a21380dbcf4ee60e2234a09bfd5f25f3fc31
Cristhian <72753117+Crissxp@users.noreply.github.com>
2021-10-25 20:05:48 -0300

    ruta, landing, carpeta con imagenes

---
+++ react-client/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../../components/Nav/Nav'
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Nav/>
+     
+      <hr />
+    </div>
+  )
+}

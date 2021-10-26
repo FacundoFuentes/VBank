167077349179ee30845cd2ef7285e3c03bf63abe
Cristhian <72753117+Crissxp@users.noreply.github.com>
2021-10-26 14:39:48 -0300

    Update index.js

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { CssBaseline } from '@nextui-org/react';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline/>
+    
     <App />
   </React.StrictMode>,
   document.getElementById('root')

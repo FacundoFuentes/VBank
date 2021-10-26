b1dbf1e65cdc1b1d0d19af14d744ddb2f6206fea
Simon Oro <simonoro1@outlook.com>
2021-10-26 15:32:26 -0300

    Account Scheme Complete

---
+++ back-client/src/models/Account.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+
+const accountSchema = new mongoose.Schema(
+    {
+        cbu: {
+            type: Number,
+            required: true
+        },
+        state: {    
+            type: Boolean,
+            required: true
+        },
+        balance: {  
+            type: Number,
+            required: true,
+        },
+
+        type: {
+            type: String,
+            required: true,
+        },
+
+        // transactions: {
+        //     type: Schema.ObjectId,
+        //     ref: 'Transaction'
+        // },
+
+        // card: {
+        //     type: Schema.ObjectId,
+        //     ref: 'Card'
+        // }
+
+    },
+    {timestamps:true}
+)
+
+
+
+const Account = mongoose.model("Account", accountSchema);
+
+module.exports = Account;

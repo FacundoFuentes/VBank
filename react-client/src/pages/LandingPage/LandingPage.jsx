df3a961a7f4e584d3cf9f1743d8b8754f5cd7f91
Ayelen <ayelen.alvarez261189@gmail.com>
2021-10-27 10:28:34 -0300

    Update LandingPage.jsx
    Update Landing

--- react-client/src/pages/LandingPage/LandingPage.jsx
+++ react-client/src/pages/LandingPage/LandingPage.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Nav from '../../components/Nav/Nav'
 import landing from '../../img/image-landing.png'
 import styled from "styled-components"
-import { Button, Text, Spacer, Link} from '@nextui-org/react';
+import { Button, Text} from '@nextui-org/react';
 import card from "../../img/Card-Landing.png"
 
 const Container = styled.div`
 display: flex;
 padding: 80px 50px;
 height: 100vh;
+justify-content: center;
 `
 const Title = styled.div`
 display: flex;
@@ -16,9 +17,10 @@ justify-content: center;
 align-items: center;
 `
 const PText = styled.div`
-height: 300px;
+height: 250px;
 display: flex;
-justify-content: center;
+flex-direction: column;
+justify-content:space-around;
 align-items: center;
 text-align: center;
 `
@@ -27,8 +29,8 @@ const ButtonDiv = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
-margin-top: 30px;
-height: 100px;
+/* margin-top: 30px;
+height: 100px; */
 `
 
  const TextContainer = styled.div`
@@ -37,11 +39,12 @@ width: 600px;
 
 const ImagenContainer = styled.div`
 text-align: end;
-width: 700px;
+width: 600px;
 ` 
 
 const Card = styled.img`
 width: 400px;
+
 `
 const TitleCard = styled.h3`
 display: flex;
@@ -53,7 +56,17 @@ const ContainerCard = styled.div`
 padding: 50px 100px;
 display: flex;
 justify-content: space-around;
+height: 500px;
 `
+const TextCard= styled.div`
+display: flex;
+flex-direction: column;
+justify-content:space-evenly;
+text-align: center;
+height: 230px;
+
+`
+
 export default function LandingPage() {
   return (
     <div>
@@ -62,24 +75,27 @@ export default function LandingPage() {
        <TextContainer>
          <Title> <Text h1 size="60px">Welcome to VBank</Text> </Title>
          <PText>
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         <Text h2 color="#2CA1DE"> We lose everything after this. Spend it (wisely). </Text>
+         <Text h5> Financial Services for Millennial Customers </Text>
          </PText>
          <ButtonDiv>
-         <Button color="#2CA1DE" > Get started </Button>
+         <Button size="large" rounded="Primary" color="#2CA1DE" > Get started </Button>
          </ButtonDiv>
        </TextContainer>
        <ImagenContainer>
-         <img width="700px"  src={landing} alt= 'img not found'/>
+         <img width="900px"  src={landing} alt= 'img not found'/>
        </ImagenContainer>
       </Container>   
 
-        <TitleCard> <Text h3 size="50px">Lorem Ipsum </Text></TitleCard>
+        <TitleCard> <Text h3 size="60px">We are the light side of money</Text></TitleCard>
         <ContainerCard>
-          <Card src={card} alt= 'img not found'/>
-          <p>
-           An app and a card
-           Mastercard to manage
-          your money much easier</p>
+           <div> 
+          <Card src={card} alt= 'img not found'/> 
+           </div>
+           <TextCard>
+          <Text small color="#333"size="30px">Just an app and a card</Text>
+          <Text h6 size="40px" color="#2CA1DE" >Simple. Transparent. And for free.</Text>
+          </TextCard>
         </ContainerCard>
 
     </div>

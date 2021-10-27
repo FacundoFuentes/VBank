81b920561977e712c7b41972f1f718fa1e6de0e7
Enzo Allamano <eallamano@gmail.com>
2021-10-27 00:20:42 -0300

    Card Schema, Card Route
    La ruta del get sería depende lo que decidamos con la cantidad de tarjetas, el schema tiene algunas cosas validadas

--- back-client/src/models/Card.js
+++ back-client/src/models/Card.js
@@ -1,25 +1,38 @@
 const mongoose = require('mongoose')
 
+function calculateDueDate(){
+    let actualDate = new Date()
+    return new Date(actualDate.getFullYear() + 5, actualDate.getMonth(), actualDate.getDay())
+}
+
 const cardSchema = new mongoose.Schema({
     cardNumber: {
         type: Number,
         required: true,
+        validate: (value) => {
+            value.toString().length === 16
+        }
     },
     startDate: {
         type: Date,
         required: true,
+        default: new Date()
     },
     dueDate:{
         type: Date,
         required: true,
+        default: calculateDueDate()
     },
     status:{
         type: String,
         enum: ['Active', 'Expired', 'Blocked',]
     },
-    securityCode: {
+    CVV: {
         type: Number,
-        required: true
+        required: true,
+        validate: (value) => {
+            value < 1000 && value > 0
+        }
     },
     account: {
         type: mongoose.Schema.ObjectId,

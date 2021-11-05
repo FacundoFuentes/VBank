1f51bacd15e5e9260277b8e11d8b663fd80d6a70
Ayelen <ayelen.alvarez261189@gmail.com>
2021-11-05 18:27:11 -0300

    Create Charge.jsx

---
+++ react-client/src/pages/Charge/Charge.jsx
@@ -0,0 +1,6 @@
+import React, {useState}from 'react'
+import styled from "styled-components"
+import { Button, Text, Input, Modal, Card} from '@nextui-org/react';
+
+export default function Charge() {
+}
